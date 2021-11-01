2e7ad405dec738770cd1f3e96cb516345e4c76b5
musai7 <musaishankar307@gmail.com>
2021-11-01 10:57:36 +0530

    added snake ladder

---
+++ src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
@@ -0,0 +1,9 @@
+package cam.snakeladdergame.gamelogics;
+
+public class SnakeLadderGame {
+	public static void main(String[] args) {
+		int player1Position = 0;
+		System.out.println("player one position " + player1Position);
+	}
+
+}
