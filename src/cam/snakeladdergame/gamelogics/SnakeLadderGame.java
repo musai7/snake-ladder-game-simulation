4926a1eaff954176acddf8b4e1ec61c4e08f3fab
musai7 <musaishankar307@gmail.com>
2021-11-01 11:30:56 +0530

    added snake ladder game for two players

--- src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
+++ src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
@@ -4,49 +4,68 @@ public class SnakeLadderGame {
 	public final static int LADDER = 1;
 	public final static int SNAKE = 2;
 
-	public static void main(String[] args) {
-		int player1Position = 0, temp = 0, diceRollTimes = 0;
-		while (true) {
-			int diceNum = (int) (Math.random() * 6 + 1);
-			diceRollTimes++;
-			System.out.println("dice value " + diceNum);
-			temp = player1Position;
-			player1Position += diceNum;
-			if (player1Position > 100) {
-				player1Position = temp;
-			}
-			if (player1Position == 100) {
-				break;
-			}
-			System.out.println("player one position " + player1Position);
-			double random = Math.random() * 3;
-			switch ((int) random) {
-			case LADDER:
-				System.out.println("player entered ladder position ");
-				player1Position += diceNum;
-				if (player1Position > 100) {
-					player1Position = temp;
-				}
-				break;
-			case SNAKE:
-				System.out.println("player entered snake position ");
-				player1Position -= diceNum;
-				temp = player1Position;
-				if (player1Position < 0) {
-					player1Position = 0;
-				}
-				break;
-			default:
-				System.out.println("player entered the no play position ");
-				break;
-			}
-			System.out.println("player one position " + player1Position);
-			if (player1Position == 100) {
-				break;
-			}
+	public int snakeLadderGame(int playerPosition, int player) {
+		int temp = 0;
+		int diceNum = (int) (Math.random() * 6 + 1);//getting random dice number
+		System.out.println("now player " + player + " turn");
+		System.out.println("dice number " + diceNum);
+		playerPosition += diceNum;
+		if (playerPosition > 100) {
+			playerPosition -= diceNum;
 		}
-		System.out.println("number of times player one roll the die : " + diceRollTimes);
-		System.out.println("player one win and position : " + player1Position);
+		if (playerPosition == 100) {
+			return playerPosition;
+		}
+		// System.out.println("player"+player+"position " + playerPosition);
+		double random = Math.random() * 3;
+		temp = playerPosition;
+		switch ((int) random) {
+		case LADDER:
+			System.out.println("player entered ladder position ");
+			playerPosition += diceNum;
+			if (playerPosition > 100) {
+				playerPosition = temp;
+			}
+			System.out.println("player get another chance to roll dice ");
+			SnakeLadderGame game = new SnakeLadderGame();
+			playerPosition = game.snakeLadderGame(playerPosition, player);
+			break;
+		case SNAKE:
+			System.out.println("player entered snake position ");
+			playerPosition -= diceNum;
+			temp = playerPosition;
+			if (playerPosition < 0) {
+				playerPosition = 0;
+			}
+			break;
+		default:
+			System.out.println("player entered the no play position ");
+			break;
+		}
+		System.out.println("player" + player + "position " + playerPosition);
+		return playerPosition;
+	}
+
+	public static void main(String[] args) {
+		int player1Position = 0, player2Position = 0, player1 = 1, player2 = 2;
+
+		SnakeLadderGame game = new SnakeLadderGame();//creating object of this class
+
+		while (true) {
+			player1Position = game.snakeLadderGame(player1Position, player1);
+			if (player1Position == 100) {
+				System.out.println("player one win the game lets celabrate ");
+				break;
+			}
+			player2Position = game.snakeLadderGame(player2Position, player2);
+			if (player2Position == 100) {
+				System.out.println("player two win the game lets celabrate ");
+				break;
+			}
+
+		}
+		System.out.println("player one position " + player1Position);
+		System.out.println("player two position " + player2Position);
 	}
 
 }
