96ca376c70495c4bea8f09c9fc7fbe7a7a59ce56
musai7 <musaishankar307@gmail.com>
2021-11-01 11:05:10 +0530

    added snake ladder game

--- src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
+++ src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
@@ -1,10 +1,27 @@
 package cam.snakeladdergame.gamelogics;
 
 public class SnakeLadderGame {
+	public final static int LADDER = 1;
+	public final static int SNAKE = 2;
+
 	public static void main(String[] args) {
 		int player1Position = 0;
-			int diceNum = (int) (Math.random() * 6 + 1);
-			System.out.println("player one position " + player1Position);
-			System.out.println("Dice number : " + diceNum);
+		int diceNum = (int) (Math.random() * 6 + 1);
+		System.out.println("player one position " + player1Position);
+		System.out.println("Dice number : " + diceNum);
+		double random = Math.random() * 3;
+		switch ((int) random) {
+		case LADDER:
+			System.out.println("player entered ladder position ");
+			player1Position += diceNum;
+			break;
+		case SNAKE:
+			System.out.println("player entered snake position ");
+			player1Position -= diceNum;
+			break;
+		default:
+			System.out.println("player entered the no play position ");
+			break;
+		}
 	}
 }
