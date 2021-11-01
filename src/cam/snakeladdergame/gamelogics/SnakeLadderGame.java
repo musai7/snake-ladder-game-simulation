52e7cd54a899ca372d129bf9c297297f4bad5287
musai7 <musaishankar307@gmail.com>
2021-11-01 11:01:03 +0530

    added snake ladder game

--- src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
+++ src/cam/snakeladdergame/gamelogics/SnakeLadderGame.java
@@ -3,7 +3,8 @@ package cam.snakeladdergame.gamelogics;
 public class SnakeLadderGame {
 	public static void main(String[] args) {
 		int player1Position = 0;
-		System.out.println("player one position " + player1Position);
+			int diceNum = (int) (Math.random() * 6 + 1);
+			System.out.println("player one position " + player1Position);
+			System.out.println("Dice number : " + diceNum);
 	}
-
 }
